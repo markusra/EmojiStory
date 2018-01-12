da1380a129dde1a4376bc857f19ba8311b8798de
martkjel <martinkjellevand@gmail.com>
2018-01-12 22:24:39 +0100

    created new questionnaire

--- src/containers/Questionnaire/index.js
+++ src/containers/Questionnaire/index.js
@@ -7,23 +7,77 @@ class Questionnaire extends Component {
     super();
     this.surveyJson = {
       pages: [
-        {
-          name: "page1",
-          questions: [
-            {
-              type: "text",
-              name: "age",
-              title: "enter age"
-            },
-            {
-              type: "text",
-              name: "gender",
-              title: "enter gender"
-            },
+       {
+        name: "page1",
+        elements: [
+         {
+          type: "radiogroup",
+          name: "gender",
+          title: "Gender:",
+          choices: [
+           "Female",
+           "Male",
+           "Other",
+           "Rather not say"
           ]
-        }
+         },
+         {
+          type: "text",
+          name: "age",
+          title: "Please enter your age:"
+         },
+         {
+          type: "dropdown",
+          choices: [
+           "item1",
+           "item2",
+           "item3"
+          ],
+          choicesByUrl: {
+           url: "https://restcountries.eu/rest/v2/all"
+          },
+          name: "country",
+          title: "Where do you come from?"
+         },
+         {
+          type: "dropdown",
+          choices: [
+           "Arts and entertainment",
+           "Business",
+           "Industrial and manufacturing",
+           "Law enforcement",
+           "Science and technology",
+           "IT",
+           "Healthcare and medicine",
+           "Service and sales",
+           "High school",
+           "Primary school",
+           "Other",
+           "Rather not say"
+          ],
+          name: "profession",
+          title: "Please select your line of work or field of study:"
+         },
+         {
+          type: "radiogroup",
+          name: "emoji-use",
+          title: "How often do you use emojis?",
+          choices: [
+           "Once a day",
+           "Several times a week",
+           "Once a week",
+           "Never"
+          ]
+         }
+        ]
+       }
       ]
-    }
+     }
+  }
+
+  componentWillMount() {    
+    Survey.Survey.cssType = "bootstrap";
+    Survey.defaultBootstrapCss.navigationButton = 'btn btn-success';
   }
 
   sendDataToDB(survey) {
@@ -37,11 +91,10 @@ class Questionnaire extends Component {
 
   render() {
     return (
-      <div className="container">
-        <Survey.Survey json={this.surveyJson} onComplete={this.sendDataToDB} />
-      </div>
+      <Survey.Survey json={this.surveyJson} onComplete={this.sendDataToDB} />
     );
   }
 }
 
 export default Questionnaire;
+
