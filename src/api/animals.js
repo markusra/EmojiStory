4b8398a83cce4152b6b1d7dee66e889c32405513
Martin Kjellevand <martinkjellevand@gmail.com>
2018-02-16 14:44:09 +0100

    Added animals, food and objects

---
+++ src/api/animals.js
@@ -0,0 +1,437 @@
+const animals = [
+  {
+    text: "crocodile",
+    src: "animals/1f40a.svg"
+  },
+  {
+    text: "whale",
+    emojiList: [
+      {
+        text: "whale1",
+        src: "animals/1f40b.svg"
+      },
+      {
+        text: "whale2",
+        src: "animals/1f433.svg"
+      }
+    ]
+  },
+  {
+    text: "snail",
+    src: "animals/1f40c.svg"
+  },
+  {
+    text: "snake",
+    src: "animals/1f40d.svg"
+  },
+  {
+    text: "horse",
+    emojiList: [
+      {
+        text: "horse1",
+        src: "animals/1f40e.svg"
+      },
+      {
+        text: "horse2",
+        src: "animals/1f434.svg"
+      }
+    ]
+  },
+  {
+    text: "ram",
+    src: "animals/1f40f.svg"
+  },
+  {
+    text: "caterpillar",
+    src: "animals/1f41b.svg"
+  },
+  {
+    text: "ant",
+    src: "animals/1f41c.svg"
+  },
+  {
+    text: "honeybee",
+    src: "animals/1f41d.svg"
+  },
+  {
+    text: "ladybug",
+    src: "animals/1f41e.svg"
+  },
+  {
+    text: "fish",
+    src: "animals/1f41f.svg"
+  },
+  {
+    text: "camel",
+    emojiList: [
+      {
+        text: "camel1",
+        src: "animals/1f42a.svg"
+      },
+      {
+        text: "camel2",
+        src: "animals/1f42b.svg"
+      }
+    ]
+  },
+  {
+    text: "dolphin",
+    src: "animals/1f42c.svg"
+  },
+  {
+    text: "cow",
+    emojiList: [
+      {
+        text: "cow1",
+        src: "animals/1f42e.svg"
+      },
+      {
+        text: "cow2",
+        src: "animals/1f404.svg"
+      }
+    ]
+  },
+  {
+    text: "tiger",
+    emojiList: [
+      {
+        text: "tiger1",
+        src: "animals/1f42f.svg"
+      },
+      {
+        text: "tiger2",
+        src: "animals/1f405.svg"
+      },
+    ]
+  },
+  {
+    text: "wolf",
+    src: "animals1f43a.svg"
+  },
+  {
+    text: "bear",
+    src: "animals/1f43b.svg"
+  },
+  {
+    text: "panda",
+    src: "animals/1f43c.svg"
+  },
+  {
+    text: "squirrelo",
+    src: "animals1f43f.svg"
+  },
+  {
+    text: "dove",
+    src: "animals/1f54a.svg"
+  },
+  {
+    text: "cat",
+    emojiList: [
+      {
+        text: "cat1",
+        src: "animals/1f63a.svg"
+      },
+      {
+        text: "cat2",
+        src: "animals/1f63b.svg"
+      },
+      {
+        text: "cat3",
+        src: "animals1f63c.svg"
+      },
+      {
+        text: "cat4",
+        src: "animals/1f63d.svg"
+      },
+      {
+        text: "cat5",
+        src: "animals/1f63e.svg"
+      },
+      {
+        text: "cat6",
+        src: "animals/1f63f.svg"
+      },
+      {
+        text: "cat7",
+        src: "animals/1f638.svg"
+      },
+      {
+        text: "cat8",
+        src: "animals/1f639.svg"
+      },
+      {
+        text: "cat9",
+        src: "animals/1f640.svg"
+      },
+      {
+        text: "cat10",
+        src: "animals/1f408.svg"
+      },
+      {
+        text: "cat11",
+        src: "animals/1f431.svg"
+      }
+    ]
+  },
+  {
+    text: "monkey",
+    emojiList: [
+      {
+        text: "monkey1",
+        src: "animals/1f64a.svg"
+      },
+      {
+        text: "monkey2",
+        src: "animals/1f435.svg"
+      },
+      {
+        text: "monkey3",
+        src: "animals/1f648.svg"
+      },
+      {
+        text: "monkey4",
+        src: "animals/1f649.svg"
+      },
+      {
+        text: "monkey5",
+        src: "animals/1f412.svg"
+      }
+    ]
+  },
+  {
+    text: "fox",
+    src: "animals/1f98a.svg"
+  },
+  {
+    text: "butterfly",
+    src: "animals/1f98b.svg"
+  },
+  {
+    text: "deer",
+    src: "animals/1f98c.svg"
+  },
+  {
+    text: "gorilla",
+    src: "animals/1f98d.svg"
+  },
+  {
+    text: "lizard",
+    src: "animals/1f98e.svg"
+  },
+  {
+    text: "rhinoceros",
+    src: "animals/1f98f.svg"
+  },
+  {
+    text: "mouse",
+    emojiList: [
+      {
+        text: "mouse 1",
+        src: "animals/1f400.svg"
+      },
+      {
+        text: "mouse 2",
+        src: "animals/1f401.svg"
+      },
+      {
+        text: "mouse 3",
+        src: "animals/1f42d.svg"
+      }
+    ]
+  },
+  {
+    text: "ox",
+    src: "animals/1f402.svg"
+  },
+  {
+    text: "buffalo",
+    src: "animals/1f403.svg"
+  },
+  {
+    text: "leopard",
+    src: "animals/1f406.svg"
+  },
+  {
+    text: "rabbit",
+    emojiList: [
+      {
+        text: "rabbit1",
+        src: "animals/1f407.svg"
+      },
+      {
+        text: "rabbit2",
+        src: "animals/1f430.svg"
+      },
+    ]
+  },
+  {
+    text: "dragon",
+    emojiList: [
+      {
+        text: "dragon1",
+        src: "animals/1f409.svg"
+      },
+      {
+        text: "dragon2",
+        src: "animals/1f432.svg"
+      },
+    ]
+  },
+  {
+    text: "goat",
+    src: "animals/1f410.svg"
+  },
+  {
+    text: "dog",
+    emojiList: [
+      {
+        text: "dog1",
+        src: "animals/1f415.svg"
+      },
+      {
+        text: "dragon2",
+        src: "animals/1f436.svg"
+      },
+    ]
+  },
+  {
+    text: "sheep",
+    src: "animals/1f411.svg"
+  },
+  {
+    text: "cock",
+    src: "animals/1f413.svg"
+  },
+  {
+    text: "chicken",
+    emojiList: [
+      {
+        text: "chicken1",
+        src: "animals/1f414.svg"
+      },
+      {
+        text: "chicken2",
+        src: "animals/1f423.svg"
+      },
+      {
+        text: "chicken3",
+        src: "animals/1f424.svg"
+      },
+      {
+        text: "chicken4",
+        src: "animals/1f425.svg"
+      }
+    ]
+  },
+  {
+    text: "pig",
+    emojiList: [
+      {
+        text: "pig1",
+        src: "animals/1f416.svg"
+      },
+      {
+        text: "pig2",
+        src: "animals/1f437.svg"
+      },
+    ]
+  },
+  {
+    text: "boar",
+    src: "animals/1f417.svg"
+  },
+  {
+    text: "elephant",
+    src: "animals/1f418.svg"
+  },
+  {
+    text: "octopus",
+    src: "animals/1f419.svg"
+  },
+  {
+    text: "clownfish",
+    src: "animals/1f420.svg"
+  },
+  {
+    text: "blowfish",
+    src: "animals/1f421.svg"
+  },
+  {
+    text: "turtle",
+    src: "animals/1f422.svg"
+  },
+  {
+    text: "bird",
+    src: "animals/1f426.svg"
+  },
+  {
+    text: "pengiun",
+    src: "animals/1f427.svg"
+  },
+  {
+    text: "koala",
+    src: "animals/1f428.svg"
+  },
+  {
+    text: "poodle",
+    src: "animals/1f429.svg"
+  },
+  {
+    text: "frog",
+    src: "animals/1f438.svg"
+  },
+  {
+    text: "spider",
+    src: "animals/1f577.svg"
+  },
+  {
+    text: "crab",
+    src: "animals/1f980.svg"
+  },
+  {
+    text: "lion",
+    src: "animals/1f981.svg"
+  },
+  {
+    text: "scorpion",
+    src: "animals/1f982.svgg"
+  },
+  {
+    text: "rooster",
+    src: "animals/1f983.svg"
+  },
+  {
+    text: "unicorn",
+    src: "animals/1f984.svg"
+  },
+  {
+    text: "eagle",
+    src: "animals/1f985.svg"
+  },
+  {
+    text: "duck",
+    src: "animals/1f986.svg"
+  },
+  {
+    text: "bat",
+    src: "animals/1f987.svg"
+  },
+  {
+    text: "shark",
+    src: "animals/1f988.svg"
+  },
+  {
+    text: "owl",
+    src: "animals/1f989.svg"
+  },
+  {
+    text: "shrimp",
+    src: "animals/1f990.svg"
+  },
+  {
+    text: "squid",
+    src: "animals/1f991.svg"
+  }
+];
+
+export default { animals };
---
+++ src/api/objects.js
@@ -0,0 +1,788 @@
+const objects = [ 
+  {
+    text: "umbrellas",
+    emojiList: [
+      {
+        text: 'umbrella1',
+        src: 'objects/1f302.svg'
+      },
+    
+      {
+        text: 'umbrella2',
+        src: 'objects/2602.svg'
+      }
+    ]
+  },
+  
+  {
+    text: 'cutlery',
+    src: 'objects/1f374.svg'
+  },
+  
+  {
+    text: 'presents',
+    src: 'objects/1f381.svg'
+  },
+  
+  {
+    text: 'ballons',
+    src: 'objects/1f388.svg'
+  },
+  
+  {
+    text: 'backpacks',
+    src: 'objects/1f392.svg'
+  },
+
+  {
+    text: "medals",
+    emojiList: [
+      {
+        text: "medals1",
+        src: "objects/1f396.svg"
+      },
+      {
+        text: 'medal2',
+        src: 'objects/1f3c5.svg'
+      },
+      {
+        text: 'medal3',
+        src: 'objects/1f947.svg'
+      },
+      
+      {
+        text: 'medal4',
+        src: 'objects/1f948.svg'
+      },
+      
+      {
+        text: 'medal5',
+        src: 'objects/1f949.svg'
+      },
+    ]
+  },
+
+  {
+    text: 'microphone',
+    src: 'objects/1f399.svg'
+  },
+
+  {
+    text: "film cameras",
+    emojiList: [
+      {
+        text: "filmcamera1",
+        src: "objects/1f3a5.svg"
+      },
+      {
+        text: "filmcamera2",
+        src: "objects/1f3a6.svg"
+      },
+      {
+        text: 'filmcamera3',
+        src: 'objects/1f4f9.svg'
+      },
+    
+      {
+        text: 'filmcamera4',
+        src: 'objects/1f4fd.svg'
+      },
+    ]
+  },
+
+  {
+    text: 'headphones',
+    src: 'objects/1f3a7.svg'
+  },
+
+  {
+    text: "hats",
+    emojiList: [
+      {
+        text: "hat1",
+        src: "objects/1f3a9.svg"
+      },
+      {
+        text: 'hat2',
+        src: 'objects/1f452.svg'
+      }
+    ]
+  },
+
+  {
+    text: 'tank tops',
+    src: 'objects/1f3bd.svg'
+  },
+  
+  
+  {
+    text: 'trophies',
+    src: 'objects/1f3c6.svg'
+  },
+  
+  {
+    text: 'flowers',
+    src: 'objects/1f3f5.svg'
+  },
+  
+  {
+    text: 'vases',
+    src: 'objects/1f3fa.svg'
+  },
+  
+  {
+    text: 'crowns',
+    src: 'objects/1f451.svg'
+  },
+  
+  {
+    text: 'glasses',
+    src: 'objects/1f453.svg'
+  },
+  
+  {
+    text: 'shirts',
+    src: 'objects/1f454.svg'
+  },
+  
+  {
+    text: 't-shirts',
+    src: 'objects/1f455.svg'
+  },
+  
+  {
+    text: 'pants',
+    src: 'objects/1f456.svg'
+  },
+  
+  {
+    text: 'dresses',
+    src: 'objects/1f457.svg'
+  },
+  
+  {
+    text: 'bikinis',
+    src: 'objects/1f459.svg'
+  },
+
+  {
+    text: "purses",
+    emojiList: [
+      {
+        text: "purses1",
+        src: "objects/1f45b.svg"
+      },
+      {
+        text: 'purses2',
+        src: 'objects/1f45c.svg'
+      },
+      {
+        text: 'purses3',
+        src: 'objects/1f45d.svg'
+      }
+    ]
+  },
+  
+  {
+    text: "shoes",
+    emojiList: [
+      {
+        text: "shoes1",
+        src: "objects/1f45e.svg"
+      },
+      {
+        text: 'shoes2',
+        src: 'objects/1f461.svg'
+      },
+      {
+        text: 'shoes3',
+        src: 'objects/1f462.svg'
+      }
+    ]
+  },
+  
+  {
+    text: 'sneakers',
+    src: 'objects/1f45f.svg'
+  },
+  
+  {
+    text: 'high heels',
+    src: 'objects/1f460.svg'
+  },
+  
+  {
+    text: 'pills',
+    src: 'objects/1f48a.svg'
+  },
+  
+  {
+    text: 'rings',
+    src: 'objects/1f48d.svg'
+  },
+  
+  {
+    text: 'diamonds',
+    src: 'objects/1f48e.svg'
+  },
+  
+  {
+    text: 'light bulbs',
+    src: 'objects/1f4a1.svg'
+  },
+  
+  {
+    text: 'water',
+    src: 'objects/1f4a7.svg'
+  },
+  
+  {
+    text: 'poop',
+    src: 'objects/1f4a9.svg'
+  },
+
+  {
+    text: "money",
+    emojiList: [
+      {
+        text: 'money1',
+        src: 'objects/1f4b0.svg'
+      },
+      {
+        text: 'money2',
+        src: 'objects/1f4b4.svg'
+      },
+      {
+        text: 'money3',
+        src: 'objects/1f4b5.svg'
+      },
+      {
+        text: 'money4',
+        src: 'objects/1f4b6.svg'
+      },
+      {
+        text: 'money5',
+        src: 'objects/1f4b7.svg'
+      },
+      {
+        text: 'money6',
+        src: 'objects/1f4b8.svg'
+      }
+    ]
+  },
+  
+  {
+    text: 'credit cards',
+    src: 'objects/1f4b3.svg'
+  },
+
+  {
+    text: "computers",
+    emojiList: [
+      {
+        text: 'computer1',
+        src: 'objects/1f4bb.svg'
+      },
+      {
+        text: 'computer2',
+        src: 'objects/1f5a5.svg'
+      },
+    ]
+  },
+  
+  {
+    text: 'briefcases',
+    src: 'objects/1f4bc.svg'
+  },
+
+  {
+    text: "CDs",
+    emojiList: [
+      {
+        text: 'CDs1',
+        src: 'objects/1f4bf.svg'
+      },
+      
+      {
+        text: 'CDs2',
+        src: 'objects/1f4c0.svg'
+      }
+    ]
+  },
+
+  {
+    text: "paper",
+    emojiList: [
+      {
+        text: 'paper1',
+        src: 'objects/1f4c3.svg'
+      },
+      
+      {
+        text: 'paper2',
+        src: 'objects/1f4c4.svg'
+      }
+    ]
+  },
+
+  {
+    text: "calendars",
+    emojiList: [
+      {
+        text: 'calendars1',
+        src: 'objects/1f4c5.svg'
+      },
+      
+      {
+        text: 'calendars2',
+        src: 'objects/1f4c6.svg'
+      },
+
+      {
+        text: 'calendars3',
+        src: 'objects/1f5d3.svg'
+      },
+    ]
+  },
+  
+  {
+    text: 'thumbtacks',
+    src: 'objects/1f4cc.svg'
+  },
+
+  {
+    text: "paper clips",
+    emojiList: [
+      {
+        text: 'paperclip1',
+        src: 'objects/1f4ce.svg'
+      },
+    
+      {
+        text: 'paperclip2',
+        src: 'objects/1f587.svg'
+      }    
+    ]
+  },
+  
+  {
+    text: 'ruler',
+    src: 'objects/1f4cf.svg'
+  },
+
+  {
+    text: "books",
+    emojiList: [
+      {
+        text: 'book1',
+        src: 'objects/1f4d3.svg'
+      },
+      
+      {
+        text: 'book2',
+        src: 'objects/1f4d4.svg'
+      },
+      
+      {
+        text: 'book3',
+        src: 'objects/1f4d5.svg'
+      },
+      
+      {
+        text: 'book4',
+        src: 'objects/1f4d6.svg'
+      },
+      
+      {
+        text: 'book5',
+        src: 'objects/1f4d7.svg'
+      },
+      
+      {
+        text: 'book6',
+        src: 'objects/1f4d8.svg'
+      },
+      
+      {
+        text: 'book7',
+        src: 'objects/1f4d9.svg'
+      },
+      
+      {
+        text: 'book8',
+        src: 'objects/1f4da.svg'
+      }
+    ]
+  },
+
+  {
+    text: "telephones",
+    emojiList: [
+      {
+        text: 'telephone1',
+        src: 'objects/1f4de.svg'
+      },
+      {
+        text: 'telephone2',
+        src: 'objects/260e.svg'
+      }
+    ]
+  },
+
+  {
+    text: "satelittes",
+    emojiList: [
+      {
+        text: 'satelitte1',
+        src: 'objects/1f4e1.svg'
+      },
+      {
+        text: 'satelitte2',
+        src: 'objects/1f6f0.svg'
+      }
+    ]
+  },
+  
+  {
+    text: 'speakers',
+    src: 'objects/1f4e2.svg'
+  },
+  
+  {
+    text: 'boxes',
+    src: 'objects/1f4e6.svg'
+  },
+
+  {
+    text: "mailbox",
+    emojiList: [
+      {
+        text: 'mailbox1',
+        src: 'objects/1f4ea.svg'
+      },
+      
+      {
+        text: 'mailbox2',
+        src: 'objects/1f4eb.svg'
+      },
+      
+      {
+        text: 'mailbox3',
+        src: 'objects/1f4ec.svg'
+      },
+      
+      {
+        text: 'mailbox4',
+        src: 'objects/1f4ed.svg'
+      },
+      {
+        text: 'mailbox5',
+        src: 'objects/1f4ee.svg'
+      }
+    ]
+  },
+  
+  {
+    text: 'newspapers',
+    src: 'objects/1f4f0.svg'
+  },
+  
+  {
+    text: 'mobile phones',
+    src: 'objects/1f4f1.svg'
+  },
+
+  {
+    text: "cameras",
+    emojiList: [
+      {
+        text: 'camera1',
+        src: 'objects/1f4f7.svg'
+      },
+      
+      {
+        text: 'camera2',
+        src: 'objects/1f4f8.svg'
+      }
+    ]
+  },
+  
+  {
+    text: 'TVs',
+    src: 'objects/1f4fa.svg'
+  },
+  
+  {
+    text: 'radios',
+    src: 'objects/1f4fb.svg'
+  },
+
+  
+  {
+    text: 'battery',
+    src: 'objects/1f50b.svg'
+  },
+
+  {
+    text: "magnifying glasses",
+    emojiList: [
+      {
+        text: 'magnifyingglass1',
+        src: 'objects/1f50d.svg'
+      },
+      
+      {
+        text: 'magnifyingglass2',
+        src: 'objects/1f50e.svg'
+      }
+    ]
+  },
+
+  {
+    text: "keys",
+    emojiList: [
+      {
+        text: 'key1',
+        src: 'objects/1f511.svg'
+      },
+    
+      {
+        text: 'key2',
+        src: 'objects/1f5dd.svg'
+      }
+    ]
+  },
+
+  {
+    text: "locks",
+    emojiList: [
+      {
+        text: 'lock1',
+        src: 'objects/1f512.svg'
+      },
+      
+      {
+        text: 'lock2',
+        src: 'objects/1f513.svg'
+      }
+    ]
+  },
+  
+  {
+    text: 'flashlights',
+    src: 'objects/1f526.svg'
+  },
+  
+  {
+    text: 'hammers',
+    src: 'objects/1f528.svg'
+  },
+
+  {
+    text: "telescopes",
+    emojiList: [
+      {
+        text: 'telescope1',
+        src: 'objects/1f52c.svg'
+      },
+      
+      {
+        text: 'telescope2',
+        src: 'objects/1f52d.svg'
+      }
+    ]
+  },
+  
+  {
+    text: 'candles',
+    src: 'objects/1f56f.svg'
+  },
+  
+  {
+    text: 'sunglasses',
+    src: 'objects/1f576.svg'
+  },
+
+  {
+    text: "pens",
+    emojiList: [
+      {
+        text: 'pen1',
+        src: 'objects/1f58a.svg'
+      },
+      
+      {
+        text: 'pen2',
+        src: 'objects/1f58b.svg'
+      }
+    ]
+  },
+  
+  {
+    text: 'paint brushes',
+    src: 'objects/1f58c.svg'
+  },
+  
+  {
+    text: 'printers',
+    src: 'objects/1f5a8.svg'
+  },
+  
+  {
+    text: 'computer mouses',
+    src: 'objects/1f5b1.svg'
+  },
+  
+  {
+    text: 'pictures',
+    src: 'objects/1f5bc.svg'
+  },
+  
+  {
+    text: 'wastebasket',
+    src: 'objects/1f5d1.svg'
+  },
+  
+  {
+    text: 'maps',
+    src: 'objects/1f5fa.svg'
+  },
+  
+  {
+    text: 'liberty statues',
+    src: 'objects/1f5fd.svg'
+  },
+  
+  {
+    text: 'doors',
+    src: 'objects/1f6aa.svg'
+  },
+  
+  {
+    text: 'toilets',
+    src: 'objects/1f6bd.svg'
+  },
+  
+  {
+    text: 'bathtubs',
+    src: 'objects/1f6c1.svg'
+  },
+  
+  {
+    text: 'couches',
+    src: 'objects/1f6cb.svg'
+  },
+  
+  {
+    text: 'bells',
+    src: 'objects/1f6ce.svg'
+  },
+
+  {
+    text: "beds",
+    emojiList: [
+      {
+        text: 'bed1',
+        src: 'objects/1f6cc.svg'
+      },
+      
+      {
+        text: 'bed2',
+        src: 'objects/1f6cf.svg'
+      },
+    ]
+  },
+  
+  {
+    text: 'shopping carts',
+    src: 'objects/1f6d2.svg'
+  },
+  
+  {
+    text: 'tools',
+    src: 'objects/1f6e0.svg'
+  },
+  
+  {
+    text: 'shields',
+    src: 'objects/1f6e1.svg'
+  },
+  
+  {
+    text: 'oil drums',
+    src: 'objects/1f6e2.svg'
+  },
+  
+  {
+    text: 'drums',
+    src: 'objects/1f941.svg'
+  },
+  
+  {
+    text: 'spoons',
+    src: 'objects/1f944.svg'
+  },
+  
+  {
+    text: 'boxing gloves',
+    src: 'objects/1f94a.svg'
+  },
+  
+  {
+    text: 'watches',
+    src: 'objects/231a.svg'
+  },
+
+  {
+    text: "hourglasses",
+    emojiList: [
+      {
+        text: 'hourglass1',
+        src: 'objects/231b.svg'
+      },
+    
+      {
+        text: 'hourglass2',
+        src: 'objects/23f3.svg'
+      }
+    ]
+  },
+  
+  {
+    text: 'keyboards',
+    src: 'objects/2328.svg'
+  },
+  
+  {
+    text: 'clocks',
+    src: 'objects/23f0.svg'
+  },
+  
+  {
+    text: 'anchors',
+    src: 'objects/2693.svg'
+  },
+  
+  {
+    text: 'skates',
+    src: 'objects/26f8.svg'
+  },
+  
+  {
+    text: 'scissors',
+    src: 'objects/2702.svg'
+  },
+  
+  {
+    text: 'letters',
+    src: 'objects/2709.svg'
+  },
+  
+  {
+    text: 'pencils',
+    src: 'objects/270f.svg'
+  },
+  
+];
+
+export default { objects };
