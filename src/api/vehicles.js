f2ac7db7d102ac894ddf9cd64f20c8a39dee6402
martkjel <martinkjellevand@gmail.com>
2018-02-16 22:08:57 +0100

    Add vehicles and weather

---
+++ src/api/vehicles.js
@@ -0,0 +1,263 @@
+const vehicles = [
+  {
+    text: "rocket",
+    src: "vehicles/1f680.svg"
+  },
+
+  {
+    text: "helicopter",
+    src: "vehicles/1f681.svg"
+  },
+
+  {
+    text: "locomotive",
+    src: "vehicles/1f682.svg"
+  },
+
+  {
+    text: "train",
+    emojiList: [
+      {
+        text: "train1",
+        src: "vehicles/1f684.svg"
+      },
+
+      {
+        text: "train2",
+        src: "vehicles/1f685.svg"
+      },
+
+      {
+        text: "train3",
+        src: "vehicles/1f686.svg"
+      },
+
+      {
+        text: "metro",
+        src: "vehicles/1f687.svg"
+      },
+
+      {
+        text: "train4",
+        src: "vehicles/1f688.svg"
+      },
+
+      {
+        text: "train5",
+        src: "vehicles/1f689.svg"
+      },
+
+      {
+        text: "train6",
+        src: "vehicles/1f69d.svg"
+      }
+    ]
+  },
+
+  {
+    text: "tram",
+    emojiList: [
+      {
+        text: "tram1",
+        src: "vehicles/1f68a.svg"
+      },
+
+      {
+        text: "tram2",
+        src: "vehicles/1f68b.svg"
+      }
+    ]
+  },
+
+  {
+    text: "bus",
+    emojiList: [
+      {
+        text: "bus1",
+        src: "vehicles/1f68c.svg"
+      },
+
+      {
+        text: "bus2",
+        src: "vehicles/1f68d.svg"
+      }
+    ]
+  },
+
+  {
+    text: "trolleybus",
+    src: "vehicles/1f68e.svg"
+  },
+
+  {
+    text: "minibus",
+    src: "vehicles/1f690.svg"
+  },
+
+  {
+    text: "ambulance",
+    src: "vehicles/1f691.svg"
+  },
+
+  {
+    text: "fire truck",
+    src: "vehicles/1f692.svg"
+  },
+
+  {
+    text: "police car",
+    emojiList: [
+      {
+        text: "policecar1",
+        src: "vehicles/1f693.svg"
+      },
+
+      {
+        text: "policecar2",
+        src: "vehicles/1f694.svg"
+      }
+    ]
+  },
+
+  {
+    text: "taxi",
+    emojiList: [
+      {
+        text: "taxi1",
+        src: "vehicles/1f695.svg"
+      },
+
+      {
+        text: "taxi2",
+        src: "vehicles/1f696.svg"
+      }
+    ]
+  },
+
+  {
+    text: "car",
+    emojiList: [
+      {
+        text: "car1",
+        src: "vehicles/1f697.svg"
+      },
+
+      {
+        text: "car2",
+        src: "vehicles/1f698.svg"
+      },
+
+      {
+        text: "car3",
+        src: "vehicles/1f699.svg"
+      }
+    ]
+  },
+
+  {
+    text: "truck",
+    src: "vehicles/1f69b.svg"
+  },
+
+  {
+    text: "tractor",
+    src: "vehicles/1f69c.svg"
+  },
+
+  {
+    text: "suspension railway",
+    src: "vehicles/1f69f.svg"
+  },
+
+  {
+    text: "ski lift",
+    emojiList: [
+      {
+        text: "skilift1",
+        src: "vehicles/1f6a0.svg"
+      },
+
+      {
+        text: "skilift2",
+        src: "vehicles/1f6a1.svg"
+      }
+    ]
+  },
+
+  {
+    text: "ship",
+    src: "vehicles/1f6a2.svg"
+  },
+
+  {
+    text: "speedboat",
+    src: "vehicles/1f6a4.svg"
+  },
+
+  {
+    text: "bicycle",
+    src: "vehicles/1f6b2.svg"
+  },
+
+  {
+    text: "motor boat",
+    src: "vehicles/1f6e5.svg"
+  },
+
+  {
+    text: "airplane",
+    emojiList: [
+      {
+        text: "airplane1",
+        src: "vehicles/1f6e9.svg"
+      },
+
+      {
+        text: "airplane2",
+        src: "vehicles/1f6eb.svg"
+      },
+
+      {
+        text: "airplane3",
+        src: "vehicles/1f6ec.svg"
+      },
+
+      {
+        text: "airplane4",
+        src: "vehicles/2708.svg"
+      }
+    ]
+  },
+
+  {
+    text: "cruise ship",
+    src: "vehicles/1f6f3.svg"
+  },
+
+  {
+    text: "kick scooter",
+    src: "vehicles/1f6f4.svg"
+  },
+
+  {
+    text: "scooter",
+    src: "vehicles/1f6f5.svg"
+  },
+
+  {
+    text: "canoe",
+    src: "vehicles/1f6f6.svg"
+  },
+
+  {
+    text: "ferry",
+    src: "vehicles/26f4.svg"
+  },
+
+  {
+    text: "sailboat",
+    src: "vehicles/26f5.svg"
+  }
+];
+
+export default { vehicles };
---
+++ src/api/weather.js
@@ -0,0 +1,208 @@
+const weather = [
+  {
+    text: "rainbows",
+    src: "weather/1f308.svg"
+  },
+
+  {
+    text: "tsunamies",
+    src: "weather/1f30a.svg"
+  },
+
+  {
+    text: "the full moon",
+    emojiList: [
+      {
+        text: "fullmoon1",
+        src: "weather/1f315.svg"
+      },
+
+      {
+        text: "fullmoon2",
+        src: "weather/1f31d.svg"
+      }
+    ]
+  },
+
+  {
+    text: "the moon",
+    emojiList: [
+      {
+        text: "moon1",
+        src: "weather/1f319.svg"
+      },
+
+      {
+        text: "moon2",
+        src: "weather/1f31b.svg"
+      },
+
+      {
+        text: "moon3",
+        src: "weather/1f31c.svg"
+      }
+    ]
+  },
+
+  {
+    text: "the sun",
+    emojiList: [
+      {
+        text: "sun1",
+        src: "weather/1f31e.svg"
+      },
+      {
+        text: "sun2",
+        src: "weather/2600.svg"
+      }
+    ]
+  },
+
+  {
+    text: "stars",
+    emojiList: [
+      {
+        text: "stars1",
+        src: "weather/1f31f.svg"
+      },
+      {
+        text: "stars2",
+        src: "weather/2b50.svg"
+      }
+    ]
+  },
+
+  {
+    text: "shooting stars",
+    emojiList: [
+      {
+        text: "shootingstars1",
+        src: "weather/1f320.svg"
+      },
+      {
+        text: "shootingstars2",
+        src: "weather/1f4ab.svg"
+      }
+    ]
+  },
+
+  {
+    text: "sunny weather",
+    emojiList: [
+      {
+        text: "sunnyweather1",
+        src: "weather/1f324.svg"
+      },
+
+      {
+        text: "sunnyweather2",
+        src: "weather/1f325.svg"
+      },
+      {
+        text: "sunnyweather3",
+        src: "weather/26c5.svg"
+      }
+    ]
+  },
+
+  {
+    text: "unpredictable weather",
+    src: "weather/1f326.svg"
+  },
+
+  {
+    text: "rain",
+    emojiList: [
+      {
+        text: "rain1",
+        src: "weather/1f327.svg"
+      },
+      {
+        text: "rain2",
+        src: "weather/1f4a6.svg"
+      },
+
+      {
+        text: "rain3",
+        src: "weather/2614.svg"
+      }
+    ]
+  },
+
+  {
+    text: "snow",
+    emojiList: [
+      {
+        text: "snow1",
+        src: "weather/1f328.svg"
+      },
+
+      {
+        text: "snow2",
+        src: "weather/2744.svg"
+      }
+    ]
+  },
+
+  {
+    text: "lightning",
+    emojiList: [
+      {
+        text: "lightning1",
+        src: "weather/1f329.svg"
+      },
+
+      {
+        text: "lightning2",
+        src: "weather/26a1.svg"
+      },
+
+      {
+        text: "lightning3",
+        src: "weather/26c8.svg"
+      }
+    ]
+  },
+
+  {
+    text: "tornados",
+    src: "weather/1f32a.svg"
+  },
+
+  {
+    text: "fog",
+    src: "weather/1f32b.svg"
+  },
+
+  {
+    text: "autumn",
+    emojiList: [
+      {
+        text: "autumn1",
+        src: "weather/1f341.svg"
+      },
+
+      {
+        text: "autumn2",
+        src: "weather/1f342.svg"
+      }
+    ]
+  },
+
+  {
+    text: "wind",
+    src: "weather/1f4a8.svg"
+  },
+
+  {
+    text: "fire",
+    src: "weather/1f525.svg"
+  },
+
+  {
+    text: "clouds",
+    src: "weather/2601.svg"
+  }
+];
+
+export default { weather };
