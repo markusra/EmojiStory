615a13de6d9c29842f4ea4e844dfb6c2162a2134
martkjel <martinkjellevand@gmail.com>
2018-02-20 20:05:53 +0100

    Add feelings

---
+++ src/api/feelings.js
@@ -0,0 +1,665 @@
+const feelings = [
+  {
+    text: "happy",
+    emojiList: [
+      {
+        text: "happy1",
+        src: "feelings/1f466-1f3fb.svg"
+      },
+
+      {
+        text: "happy2",
+        src: "feelings/1f466-1f3fc.svg"
+      },
+
+      {
+        text: "happy3",
+        src: "feelings/1f466-1f3fd.svg"
+      },
+
+      {
+        text: "happy4",
+        src: "feelings/1f466-1f3fe.svg"
+      },
+
+      {
+        text: "happy5",
+        src: "feelings/1f466-1f3ff.svg"
+      },
+
+      {
+        text: "happy6",
+        src: "feelings/1f466.svg"
+      },
+
+      {
+        text: "happy7",
+        src: "feelings/1f467-1f3fb.svg"
+      },
+
+      {
+        text: "happy8",
+        src: "feelings/1f467-1f3fc.svg"
+      },
+
+      {
+        text: "happy9",
+        src: "feelings/1f467-1f3fd.svg"
+      },
+
+      {
+        text: "happy10",
+        src: "feelings/1f467-1f3fe.svg"
+      },
+
+      {
+        text: "happy11",
+        src: "feelings/1f467-1f3ff.svg"
+      },
+
+      {
+        text: "happy12",
+        src: "feelings/1f467.svg"
+      },
+      {
+        text: "happy13",
+        src: "feelings/1f600.svg"
+      },
+
+      {
+        text: "happy14",
+        src: "feelings/1f601.svg"
+      },
+      {
+        text: "happy15",
+        src: "feelings/1f60a.svg"
+      },
+
+      {
+        text: "happy16",
+        src: "feelings/1f60b.svg"
+      },
+      {
+        text: "happy17",
+        src: "feelings/1f642.svg"
+      },
+
+      {
+        text: "happy18",
+        src: "feelings/1f643.svg"
+      },
+      {
+        text: "happy19",
+        src: "feelings/1f917.svg"
+      },
+
+      {
+        text: "happy20",
+        src: "feelings/1f923.svg"
+      }
+    ]
+  },
+
+  {
+    text: "unhappy",
+    emojiList: [
+      {
+        text: "unhappy1",
+        src: "feelings/1f641.svg"
+      },
+      {
+        text: "unhappy2",
+        src: "feelings/1f64d-1f3fb.svg"
+      },
+
+      {
+        text: "unhappy3",
+        src: "feelings/1f64d-1f3fc.svg"
+      },
+
+      {
+        text: "unhappy4",
+        src: "feelings/1f64d-1f3fd.svg"
+      },
+
+      {
+        text: "unhappy5",
+        src: "feelings/1f64d-1f3fe.svg"
+      },
+
+      {
+        text: "unhappy6",
+        src: "feelings/1f64d-1f3ff.svg"
+      },
+
+      {
+        text: "unhappy7",
+        src: "feelings/1f64d.svg"
+      }
+    ]
+  },
+
+  {
+    text: "love",
+    emojiList: [
+      {
+        text: "love1",
+        src: "feelings/1f49a.svg"
+      },
+
+      {
+        text: "love2",
+        src: "feelings/1f49b.svg"
+      },
+
+      {
+        text: "love3",
+        src: "feelings/1f49c.svg"
+      },
+
+      {
+        text: "love4",
+        src: "feelings/1f49d.svg"
+      },
+
+      {
+        text: "love5",
+        src: "feelings/1f49e.svg"
+      },
+
+      {
+        text: "love6",
+        src: "feelings/1f49f.svg"
+      },
+      {
+        text: "love7",
+        src: "feelings/1f60d.svg"
+      }
+    ]
+  },
+
+  {
+    text: "overjoyed",
+    src: "feelings/1f602.svg"
+  },
+
+  {
+    text: "glad",
+    emojiList: [
+      {
+        text: "glad1",
+        src: "feelings/1f603.svg"
+      },
+
+      {
+        text: "glad2",
+        src: "feelings/1f604.svg"
+      }
+    ]
+  },
+
+  {
+    text: "uncomfortable",
+    src: "feelings/1f605.svg"
+  },
+
+  {
+    text: "funny",
+    src: "feelings/1f606.svg"
+  },
+
+  {
+    text: "kind",
+    src: "feelings/1f607.svg"
+  },
+
+  {
+    text: "sneaky",
+    src: "feelings/1f608.svg"
+  },
+
+  {
+    text: "playful",
+    src: "feelings/1f609.svg"
+  },
+
+  {
+    text: "satisfied",
+    src: "feelings/1f60c.svg"
+  },
+
+  {
+    text: "cool",
+    src: "feelings/1f60e.svg"
+  },
+
+  {
+    text: "smug",
+    src: "feelings/1f60f.svg"
+  },
+
+  {
+    text: "unemotional",
+    emojiList: [
+      {
+        text: "unemotional1",
+        src: "feelings/1f610.svg"
+      },
+      {
+        text: "unemotional2",
+        src: "feelings/1f636.svg"
+      }
+    ]
+  },
+
+  {
+    text: "hopeless",
+    src: "feelings/1f611.svg"
+  },
+
+  {
+    text: "irritated",
+    src: "feelings/1f612.svg"
+  },
+
+  {
+    text: "stressed",
+    src: "feelings/1f613.svg"
+  },
+
+  {
+    text: "depressed",
+    src: "feelings/1f614.svg"
+  },
+
+  {
+    text: "sad",
+    emojiList: [
+      {
+        text: "sad2",
+        src: "feelings/1f615.svg"
+      },
+
+      {
+        text: "sad1",
+        src: "feelings/1f625.svg"
+      },
+
+      {
+        text: "sad3",
+        src: "feelings/1f61e.svg"
+      },
+
+      {
+        text: "sad4",
+        src: "feelings/1f622.svg"
+      },
+
+      {
+        text: "sad5",
+        src: "feelings/1f62d.svg"
+      }
+    ]
+  },
+
+  {
+    text: "disgusted",
+    src: "feelings/1f616.svg"
+  },
+
+  {
+    text: "",
+    src: "feelings/1f617.svg"
+  },
+
+  {
+    text: "affectionate",
+    src: "feelings/1f618.svg"
+  },
+
+  {
+    text: "",
+    src: "feelings/1f619.svg"
+  },
+
+  {
+    text: "",
+    src: "feelings/1f61a.svg"
+  },
+
+  {
+    text: "silly",
+    emojiList: [
+      {
+        text: "silly1",
+        src: "feelings/1f61b.svg"
+      },
+
+      {
+        text: "silly2",
+        src: "feelings/1f61c.svg"
+      },
+
+      {
+        text: "silly3",
+        src: "feelings/1f61d.svg"
+      }
+    ]
+  },
+
+  {
+    text: "worried",
+    src: "feelings/1f61f.svg"
+  },
+
+  {
+    text: "angry",
+    emojiList: [
+      {
+        text: "angry1",
+        src: "feelings/1f620.svg"
+      },
+
+      {
+        text: "angry2",
+        src: "feelings/1f621.svg"
+      },
+
+      {
+        text: "angry3",
+        src: "feelings/1f623.svg"
+      }
+    ]
+  },
+
+  {
+    text: "furious",
+    src: "feelings/1f624.svg"
+  },
+
+  {
+    text: "surprised",
+    emojiList: [
+      {
+        text: "surprised1",
+        src: "feelings/1f626.svg"
+      },
+
+      {
+        text: "surprised2",
+        src: "feelings/1f627.svg"
+      },
+
+      {
+        text: "surprised3",
+        src: "feelings/1f62e.svg"
+      },
+
+      {
+        text: "surprised4",
+        src: "feelings/1f62f.svg"
+      }
+    ]
+  },
+
+  {
+    text: "shocked",
+    emojiList: [
+      {
+        text: "shocked1",
+        src: "feelings/1f628.svg"
+      },
+      {
+        text: "shocked2",
+        src: "feelings/1f632.svg"
+      },
+
+      {
+        text: "shocked3",
+        src: "feelings/1f64e-1f3fb.svg"
+      },
+
+      {
+        text: "shocked4",
+        src: "feelings/1f64e-1f3fc.svg"
+      },
+
+      {
+        text: "shocked5",
+        src: "feelings/1f64e-1f3fd.svg"
+      },
+
+      {
+        text: "shocked6",
+        src: "feelings/1f64e-1f3fe.svg"
+      },
+
+      {
+        text: "shocked7",
+        src: "feelings/1f64e-1f3ff.svg"
+      },
+
+      {
+        text: "shocked8",
+        src: "feelings/1f64e.svg"
+      }
+    ]
+  },
+
+  {
+    text: "angry",
+    emojiList: [
+      {
+        text: "angry1",
+        src: "feelings/1f620.svg"
+      },
+
+      {
+        text: "angry2",
+        src: "feelings/1f621.svg"
+      },
+
+      {
+        text: "angry3",
+        src: "feelings/1f623.svg"
+      }
+    ]
+  },
+
+  {
+    text: "tired",
+    src: "feelings/1f629.svg"
+  },
+
+  {
+    text: "exhausted",
+    src: "feelings/1f62b.svg"
+  },
+
+  {
+    text: "nervous",
+    src: "feelings/1f62c.svg"
+  },
+
+  {
+    text: "concerned",
+    src: "feelings/1f630.svg"
+  },
+
+  {
+    text: "scared",
+    src: "feelings/1f631.svg"
+  },
+
+  {
+    text: "sleepy",
+    src: "feelings/1f634.svg"
+  },
+
+  {
+    text: "sick",
+    emojiList: [
+      {
+        text: "sick1",
+        src: "feelings/1f637.svg"
+      },
+
+      {
+        text: "sick2",
+        src: "feelings/1f912.svg"
+      },
+
+      {
+        text: "sick3",
+        src: "feelings/1f922.svg"
+      },
+
+      {
+        text: "sick4",
+        src: "feelings/1f927.svg"
+      }
+    ]
+  },
+
+  {
+    text: "annoyed",
+    src: "feelings/1f644.svg"
+  },
+
+  {
+    text: "",
+    src: "feelings/1f645-1f3fb.svg"
+  },
+
+  {
+    text: "",
+    src: "feelings/1f645-1f3fc.svg"
+  },
+
+  {
+    text: "",
+    src: "feelings/1f645-1f3fd.svg"
+  },
+
+  {
+    text: "",
+    src: "feelings/1f645-1f3fe.svg"
+  },
+
+  {
+    text: "",
+    src: "feelings/1f645-1f3ff.svg"
+  },
+
+  {
+    text: "",
+    src: "feelings/1f645.svg"
+  },
+
+  {
+    text: "helpless",
+    src: "feelings/1f910.svg"
+  },
+
+  {
+    text: "rich",
+    src: "feelings/1f911.svg"
+  },
+
+  {
+    text: "smart",
+    src: "feelings/1f913.svg"
+  },
+
+  {
+    text: "sceptical",
+    src: "feelings/1f914.svg"
+  },
+
+  {
+    text: "hurt",
+    src: "feelings/1f915.svg"
+  },
+
+  {
+    text: "mesmerized",
+    src: "feelings/1f924.svg"
+  },
+
+  {
+    text: "",
+    src: "feelings/1f925.svg"
+  },
+
+  {
+    text: "disappointed",
+    emojiList: [
+      {
+        text: "disappointed1",
+        src: "feelings/1f926-1f3fb.svg"
+      },
+
+      {
+        text: "disappointed2",
+        src: "feelings/1f926-1f3fc.svg"
+      },
+
+      {
+        text: "disappointed3",
+        src: "feelings/1f926-1f3fd.svg"
+      },
+
+      {
+        text: "disappointed4",
+        src: "feelings/1f926-1f3fe.svg"
+      },
+
+      {
+        text: "disappointed5",
+        src: "feelings/1f926-1f3ff.svg"
+      },
+
+      {
+        text: "disappointed6",
+        src: "feelings/1f926.svg"
+      }
+    ]
+  },
+
+  {
+    text: "uncertain",
+    emojiList: [
+      {
+        text: "uncertain1",
+        src: "feelings/1f937-1f3fb.svg"
+      },
+
+      {
+        text: "uncertain2",
+        src: "feelings/1f937-1f3fc.svg"
+      },
+
+      {
+        text: "uncertain3",
+        src: "feelings/1f937-1f3fd.svg"
+      },
+
+      {
+        text: "uncertain4",
+        src: "feelings/1f937-1f3fe.svg"
+      },
+
+      {
+        text: "uncertain5",
+        src: "feelings/1f937-1f3ff.svg"
+      },
+
+      {
+        text: "uncertain6",
+        src: "feelings/1f937.svg"
+      }
+    ]
+  }
+];
+
+export default { feelings };
